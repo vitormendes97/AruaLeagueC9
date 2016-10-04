dd47aff771756a5fe9c96b552195fe395367869d
Vitor Mendes <31517900@mackenzista.com.br>
2016-10-04 01:46:14 +0000

    Home

--- application/views/HeadNovo.php
+++ application/views/HeadNovo.php
@@ -4,9 +4,13 @@
             <meta charset="utf-8">
              <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
              <script type="text/javascript" src="<?php echo base_url('/public/css/bootstrap/bootstrap.js'); ?>"></script>
+             <script type="text/javascript" src="<?php echo base_url('/public/carousel/owl-carousel/owl.carousel.js'); ?>"></script>
              <link rel="stylesheet" type="text/css" href="<?php echo base_url('/public/css/oficial.css') ?>"> 
              <link rel="stylesheet" type="text/css" href="<?php echo base_url('/public/css/OficialMobile.css') ?>"> 
             <link rel="stylesheet" type="text/css" href="<?php echo base_url('/public/css/bootstrap/bootstrap.css'); ?>">
+            <link rel="stylesheet" type="text/css" href="<?php echo base_url('/public/carousel/owl-carousel/owl.carousel.css'); ?>">
+             <link rel="stylesheet" type="text/css" href="<?php echo base_url('/public/carousel/owl-carousel/owl.theme.css'); ?>">
+            <link href="https://fonts.googleapis.com/css?family=Titillium+Web:700" rel="stylesheet">
              <meta content='width=device-width, initial-scale=1' name='viewport'/>
             
         </head>
--- application/views/home.php
+++ application/views/home.php
@@ -1,5 +1,67 @@
 <section>
     <div class="container">
-       <hr class="hr-black"></hr>
+
+        <div class="row">
+            <div class="col-md-12">
+                <p class="news">Últimas Atualizações</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <hr class="hr-black"></hr>
+            </div>
+        </div>
+        <div class="row center-block">
+            <div class="col-md-3 col-xs-3">
+                <a href="#conteudo1" class="thumbnail" data-toggle="tab">
+                    <img  src="<?php echo base_url('/public/img/arua-logo.jpg');?>" alt="...">
+                </a>
+
+            </div>
+            <div class="col-md-3 col-xs-3">
+                <a href="#conteudo2" class="thumbnail" data-toggle="tab">
+                    <img src="<?php echo base_url('/public/img/arua-logo.jpg');?>" alt="...">
+                </a>
+            </div>
+            <div class="col-md-3 col-xs-3">
+                <a href="#" class="thumbnail">
+                    <img src="<?php echo base_url('/public/img/arua-logo.jpg');?>" alt="...">
+                </a>
+            </div>
+            <div class="col-md-3 col-xs-3">
+                <a href="#" class="thumbnail">
+                    <img src="<?php echo base_url('/public/img/arua-logo.jpg');?>" alt="...">
+                </a>
+            </div>
+
+            <div class="tab-content">
+                <div class="tab-pane " id="conteudo1">Conteudo 1</div>
+                <div class="tab-pane active" id="conteudo2">
+                    <h1>Título</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non tellus eros. Vestibulum odio purus, faucibus a sagittis in, tristique ac lacus. Nunc est metus, rutrum eget imperdiet quis, interdum ac massa. Mauris sollicitudin, turpis
+                        ut tristique pharetra, felis odio blandit nulla, sed hendrerit diam nisi non dolor. Nulla velit risus, auctor nec mauris a, bibendum tempor quam. Quisque iaculis sed leo vel vulputate. Nunc eleifend, metus ac dictum porta, enim
+                        ipsum lobortis purus, at euismod urna arcu eget massa. Proin ligula tortor, malesuada tincidunt ipsum ut, bibendum tempus purus. Maecenas a tempor dui, vel ultricies nunc. Nam quis egestas leo. Cras dictum efficitur enim, et eleifend
+                        arcu pharetra in.</p>
+                </div>
+            </div>
+
+        </div>
     </div>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <p class="news">Estatísticas da Semana</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <hr class="hr-black"></hr>
+            </div>
+        </div>
+    </div>
+
 </section>
